cb5e1f0409c1ffc53cece2652da6128fd653475f
sbyim <ysb06@hotmail.com>
2024-12-02 16:30:22 +0900

    Update scaner.py

--- mllm-sensor-server/src/sensor_server/scaner.py
+++ mllm-sensor-server/src/sensor_server/scaner.py
@@ -1,14 +1,15 @@
 import logging
 import socket
 import struct
-from typing import List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple
 import threading
 import time
+from collections import deque
 
 from sensor_server.server import SERVER_IP
 from .server import SensorServer
 
-SCANER_SERVER_IP = "192.168.1.6"
+SCANER_SERVER_IP = "192.168.0.100"
 SCANER_SERVER_PORT = 46012
 DATA_VALUE_SIZE = 8  # 32-bit float
 
@@ -21,34 +22,55 @@ class ScanerFilterServer(SensorServer):
         filter_ip: str = SCANER_SERVER_IP,
         filter_port: int = SCANER_SERVER_PORT,
         data_size: int = DATA_VALUE_SIZE,
+        timeout: float = 3,
     ) -> None:
         super().__init__()
-        logger.info(f"ScanerFilterServer: {filter_ip}:{filter_port}")
+        print(f"ScanerFilterServer: {filter_ip}:{filter_port}")
         self.data_size = data_size
 
         self.filter_thread: Optional[threading.Thread] = None
         self.filter_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.filter_address = (filter_ip, filter_port)
-        self.current_scaner_data: List[float] = []
-
-    def _process_client_request(self, data: bytes) -> bytes:
-        return data
+        self.timeout = timeout
+        
+        self._scaner_steering_angles = deque([0.0] * 20, maxlen=20)
+        self._scaner_speeds = deque([0.0] * 20, maxlen=20)
 
     def _update_scaner_data(self) -> None:
         print("Updating scaner data...")
         while self.is_active:
             try:
                 data, _ = self.filter_udp_socket.recvfrom(self.buffer_size)
-                print(f"Received data: {data.hex()}")
+                values = struct.unpack("<" + "d" * (len(data) // self.data_size), data)
+                results = {}
+                key = -1
+                for idx, value in enumerate(values):
+                    if idx % 2 == 0:
+                        key = int(value)
+                    else:
+                        results[key] = value
+                self._process_scaner_data(results)
             except socket.timeout:
                 print("(Timeout) Waiting for messages...")
                 continue
             except OSError as e:
                 print(f"The socket may be forcibly closed: {e}")
 
+    def _process_client_request(self, _: bytes) -> bytes:
+        result = ""
+        result += f"[Steering Angles]:{list(self._scaner_steering_angles)}\n"
+        result += f"[Velocities]:{list(self._scaner_speeds)}\n"
+        
+        return result.encode()
+    
+    def _process_scaner_data(self, data: Dict[int, float]) -> bytes:
+        self._scaner_steering_angles.append(data[167])  # 167: Steering Angle
+        self._scaner_speeds.append(data[120])           # 120: Speed
+    
     def activate(self):
         super().activate()
         self.filter_udp_socket.bind(self.filter_address)
+        self.filter_udp_socket.settimeout(self.timeout)
         self.filter_thread = threading.Thread(target=self._update_scaner_data)
         self.filter_thread.start()
         
